16545be8936a6ad46f2b29b850801636b520316e
Iulian Crisan <iuliancrisan01@gmail.com>
2023-06-23 11:15:23 +0300

    Options button

--- src/components/link/LinkRow.tsx
+++ src/components/link/LinkRow.tsx
@@ -245,6 +245,23 @@ function LinkRow(props: Props) {
           })}
 
           <p className="ml-auto text-xs text-gray-800">{createdAtText}</p>
+
+          <div onClick={props.onContextMenu} className="p-2 !ml-0 !-mr-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-5 h-5 text-gray-600"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+              />
+            </svg>
+          </div>
         </div>
       )}
     </div>
